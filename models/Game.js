04447321837a9a283f5919119a3b306f8cef1f6c
coltonsmith135 <115854214+coltonsmith135@users.noreply.github.com>
2023-02-23 20:47:32 -0800

    added code for user and game models

---
+++ models/Game.js
@@ -0,0 +1,45 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Game extends Model {}
+
+Game.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    release_date: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+    rating: {
+      type: DataTypes.DECIMAL,
+      allowNull: true,
+    },
+    platform: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "game",
+  }
+);
